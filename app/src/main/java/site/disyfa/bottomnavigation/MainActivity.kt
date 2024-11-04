137ac0d843c54ecd559105ee9e614539518b13bd
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-11-04 22:14:27 +0700

    Move BottomNavigation project to its branch

---
+++ app/src/main/java/site/disyfa/bottomnavigation/MainActivity.kt
@@ -0,0 +1,37 @@
+package site.disyfa.bottomnavigation
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import site.disyfa.bottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        with(binding) {
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menus,menu)
+        return true
+    }
+}
